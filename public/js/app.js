38a129f2902294da985a3ae767d1bb7b529f2a5a
Eric <makingentropy@gmail.com>
2017-09-14 17:48:21 -0500

    Boilerplate for Portfolio added

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+const app = angular.module("AppJS",[]);
+
+app.controller("appjsController", ['$http', function($http){
+  const controller = this;
+  this.test="portfolio test good";
+
+
+
+}]); //end rtController
